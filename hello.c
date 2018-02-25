c01272a3f52e1fadc6cc8b3fbbd1de1b100581cd
pardhu225 <chowdary1969@gmail.com>
2018-02-25 11:48:19 +0530

    new function added user2

--- hello.c
+++ hello.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 
 void microkernel_sendmsg(char *);
+void microkernel_getmsg(char *);
 
-void main(){
+void main() {
 	printf("Helloworld!\n");
+	printf("This must be a monolithic design\n");
 	microkernel_sendmsg("is more portable");
 }
 
-void microkernel_sendmsg(char *a){
+void microkernel_sendmsg(char *a) {
 	printf("microkernel: %s\n", a);
-}
+}
+
+void microkernel_getmsg(char *b) {
+	//TODO: getmsg feature
+}
